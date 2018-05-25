318f48866faf429c28a5893b1c9edb8e0d55df89
jahav <havlicek.honza@gmail.com>
2018-05-25 18:42:47 +0200

    Initial commit adding a Service project.

---
+++ Service/DelayService.svc.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode =InstanceContextMode.Single)]
+    public class CalculatorService : IDelayService
+    {
+        public async Task<int> Delay(int value)
+        {
+            await Task.Delay(5000);
+            return value;
+        }
+    }
+}
